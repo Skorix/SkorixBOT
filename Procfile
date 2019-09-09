a54f735e319444e4728e774956da9b7b3de1ac2f
Slamer <Slamer1703@mail.ru>
2019-09-09 04:43:36 +0300

    first commit

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python SL.py
+worker: python SL.py
--- SL.py
+++ SL.py
@@ -1,260 +1,245 @@
 import telebot
 import database
 
-menu=telebot.types.ReplyKeyboardMarkup(True, False)
-menu.row('Расписание')
-menu.row('Изменения')
-menu.row('Домашка')
 
-bot=telebot.TeleBot("945462714:AAH0ikBMSiiiwjDBOafR_ZA_5D_o_jW_cPo")
+bot = telebot.TeleBot('945462714:AAH0ikBMSiiiwjDBOafR_ZA_5D_o_jW_cPo')
+	
+   
+menu = telebot.types.ReplyKeyboardMarkup(True, False)
+menu.row('Понедельник','Вторник')
+menu.row('Среда','Четверг')
+menu.row('Пятница','Субота')
+menu.row('Изменения')
+
 
 @bot.message_handler(content_types=['text'])
-def start(message):
-
-    if message.text=='/start':
-        bot.send_message(message.from_user.id,'Привет, что тебе нужно?',reply_markup=menu)
-
-    if message.text=='Расписание':    
-        keyboard=telebot.types.ReplyKeyboardMarkup(True, False)
-        keyboard.row('Понедельник','Вторник')
-        keyboard.row('Среда','Четверг')
-        keyboard.row('Пятница','Субота')
-        keyboard.row('Назад')
-        bot.send_message(message.from_user.id,'На какой день недели?',reply_markup=keyboard)
-
-
-    if message.text=='Назад':
-        bot.send_message(message.from_user.id,'Привет, что тебе нужно?',reply_markup=menu)
-
-
-    if message.text=='Понедельник' and database.chet == 'Чет':
-        bot.send_message(message.chat.id,database.mon2)
-
-    if message.text=='Понедельник' and database.chet == 'Нечет':
-        bot.send_message(message.chat.id,database.mon1)
-
-    if message.text=='Вторник' and database.chet == 'Чет':
-        bot.send_message(message.chat.id,database.tues2)
-
-    if message.text=='Вторник' and database.chet == 'Нечет':
-        bot.send_message(message.chat.id,database.tues1)
-
-    if message.text=='Среда' and database.chet == 'Чет':
-        bot.send_message(message.chat.id,database.wed2)
-
-    if message.text=='Среда' and database.chet == 'Нечет':
-        bot.send_message(message.chat.id,database.wed1)
-
-    if message.text=='Четверг' and database.chet == 'Чет':
-        bot.send_message(message.chat.id,database.thurs2)
-
-    if message.text=='Четверг' and database.chet == 'Нечет':
-        bot.send_message(message.chat.id,database.thurs1)
-
-    if message.text=='Пятница' and database.chet == 'Чет':
-        bot.send_message(message.chat.id,database.fri2)
-
-    if message.text=='Пятница' and database.chet == 'Нечет':
-        bot.send_message(message.chat.id,database.fri1)
-
-    if message.text== 'Субота' and database.chet == 'Чет':
-        bot.send_message(message.chat.id,database.sat2)
-
-    if message.text== 'Субота' and database.chet == 'Нечет':
-        bot.send_message(message.chat.id,database.sat1)
-
-
-    if message.text=='Четн':
-        sent_1 = bot.send_message(message.chat.id,'Введи чётность')
-        bot.register_next_step_handler(sent_1, chet1)
-
-
-    if message.text=='Домашка':
-        keyboard=telebot.types.ReplyKeyboardMarkup(True, False)
-        keyboard.row('Основы алгоритмов и программирования')
-        keyboard.row('Устройство и функционирование ИС')
-        keyboard.row('Элементы высшей математики')
-        keyboard.row('Русский язык и культура речи')
-        keyboard.row('Инглишь')
-        keyboard.row('Физра')
-        keyboard.row('МДК')
-        keyboard.row('БЖ')
-        keyboard.row('Назад')
-        bot.send_message(message.chat.id,'На какой предмет?',reply_markup=keyboard)
-
-    if message.text=='Основы алгоритмов и программирования':
-        bot.send_message(message.chat.id,database.les1)
-
-    if message.text=='Устройство и функционирование ИС':
-        bot.send_message(message.chat.id,database.les2) 
-
-    if message.text=='Элементы высшей математики':
-        bot.send_message(message.chat.id,database.les3)
-
-    if message.text=='Русский язык и культура речи':
-        bot.send_message(message.chat.id,database.les4)
-
-    if message.text=='Инглишь':
-        bot.send_message(message.chat.id,database.les5)
-
-    if message.text=='Физра':
-        bot.send_message(message.chat.id,database.les6)
-
-    if message.text=='МДК':
-        bot.send_message(message.chat.id,database.les7)
-
-    if message.text=='БЖ':
-        bot.send_message(message.chat.id,database.les8)                
-
-
-    if message.text=='Админ на месте':
-        keyboard=telebot.types.ReplyKeyboardMarkup(True, False)
-        keyboard.row('#Основы алгоритмов и программирования#')
-        keyboard.row('#Устройство и функционирование ИС#')
-        keyboard.row('#Элементы высшей математики#')
-        keyboard.row('#Русский язык и культура речи#')
-        keyboard.row('#Инглишь#')
-        keyboard.row('#Физра#')
-        keyboard.row('#МДК#')
-        keyboard.row('#БЖ#')
-        keyboard.row('Назад')
-        bot.send_message(message.chat.id,'На какой предмет?',reply_markup=keyboard)
-
-    if message.text=='Основы алгоритмов и программирования#':
-        sent = bot.send_message(message.chat.id,'Введи задание')
-        bot.register_next_step_handler(sent,les1)
-
-    if message.text=='Устройство и функционирование ИС#':
-        sent = bot.send_message(message.chat.id,'Введи задние')
-        bot.register_next_step_handler(sent,les2)
-
-    if message.text=='Элементы высшей математики#':
-        sent = bot.send_message(message.chat.id,'Введи задние')
-        bot.register_next_step_handler(sent,les3)
-
-    if message.text=='Русский язык и культура речи#':
-        sent = bot.send_message(message.chat.id,'Введи задние')
-        bot.register_next_step_handler(sent,les4)
-
-    if message.text=='Инглишь#':
-        sent = bot.send_message(message.chat.id,'Введи задние')
-        bot.register_next_step_handler(sent,les5)
-
-    if message.text=='Физра#':
-        sent = bot.send_message(message.chat.id,'Введи задние')
-        bot.register_next_step_handler(sent,les6)
-
-    if message.text=='МДК#':
-        sent = bot.send_message(message.chat.id,'Введи задние')
-        bot.register_next_step_handler(sent,les7)
-
-    if message.text=='БЖ#':
-        sent = bot.send_message(message.chat.id,'Введи задние')
-        bot.register_next_step_handler(sent,les8)
-
-
-    if message.text=='Чг':
-        sent = bot.send_message(message.chat.id,'Введи изменения')
-        bot.register_next_step_handler(sent,change1)
-
-    if message.text=='Изменения':
-        bot.send_message(message.chat.id,database.change)
-
-def les1(message):
-    database.les1=message.text
-    return database.les1
-
-def les2(message):
-    database.les2=message.text
-    return database.les2
-
-def les3(message):
-    database.les3=message.text
-    return database.les3
-
-def les4(message):
-    database.les4=message.text
-    return database.les4
-
-def les5(message):
-    database.les5=message.text
-    return database.les5
-
-def les6(message):
-    database.les6=message.text
-    return database.les6
-
-def les7(message):
-    database.les7=message.text
-    return database.les7
-
-def les8(message):
-    database.les8=message.text
-    return database.les8
-
-def mon1(message):
-    database.mon1=message.text
-    return database.mon1
-
-def tues1(message):
-    database.tues1=message.text
-    return database.tues1
-
-def wed1(message):
-    database.wed1=message.text
-    return database.wed1
-
-def thurs1(message):
-    database.thurs1=message.text
-    return database.thurs1
-
-def wed1(message):
-    database.wed1=message.text
-    return database.wed1
-
-def fri1(message):
-    database.fri1=message.text
-    return database.fri1
-
-def sat1(message):
-    database.sat1 =message.text
-    return database.sat1 
-
-
-
-def mon2(message):
-    database.mon1=message.text
-    return database.mon2
-
-def tues2(message):
-    database.tues1=message.text
-    return database.tues2
-
-def wed2(message):
-    database.wed1=message.text
-    return database.wed2
-
-def thurs2(message):
-    database.thurs1=message.text
-    return database.thurs2
-
-def wed2(message):
-    database.wed1=message.text
-    return database.wed2
-
-def fri2(message):
-    database.fri1=message.text
-    return database.fri2
-
-def sat2(message):
-    database.sat1 =message.text
-    return database.sat2
-
-def chet1(message):
-    database.chet =message.text
-    return database.chet
+def main(message):
+	
+
+	if message.text == '/start':
+		bot.send_message(message.from_user.id, 'На какой день недели тебе нужно рассписание?', reply_markup = menu)
+
+
+	if message.text=='Четн':
+		sent = bot.send_message(message.chat.id,'Введи чётность')
+		bot.register_next_step_handler(sent, chet)
+
+
+	if message.text == 'Понедельник' and database.chet == 'Чет':
+		Keyboard = telebot.types.ReplyKeyboardMarkup(True, False)
+		Keyboard.row('23 - Элементы высшей математики')
+		Keyboard.row('301/2 - МДК')
+		Keyboard.row('310/3 - Устройство и функционирование ИС')
+		Keyboard.row('Назад')
+		bot.send_message(message.from_user.id, 'Понедельник', reply_markup = Keyboard)
+
+	if message.text == 'Вторник' and database.chet == 'Чет':
+		Keyboard = telebot.types.ReplyKeyboardMarkup(True, False)
+		Keyboard.row('113 - Основы алгоритмизации и программирования')
+		Keyboard.row('401 - Физра')
+		Keyboard.row('22 - Элементы высшей математики')
+		Keyboard.row('Назад')
+		bot.send_message(message.from_user.id, 'Вторник', reply_markup = Keyboard)
+
+	if message.text == 'Среда' and database.chet == 'Чет':
+		Keyboard = telebot.types.ReplyKeyboardMarkup(True, False)
+		Keyboard.row('301/2 - Устройство и функционирование ИС')
+		Keyboard.row('401 - Физра')
+		Keyboard.row('22 - Элементы высшей математики')
+		Keyboard.row('Назад')
+		bot.send_message(message.from_user.id, 'Среда', reply_markup = Keyboard)
+
+	if message.text == 'Четверг' and database.chet == 'Чет':
+		Keyboard = telebot.types.ReplyKeyboardMarkup(True, False)
+		Keyboard.row('Нет-МДК')
+		Keyboard.row('107/5 - Инглишь')
+		Keyboard.row('309 - Русский язык и культура речи')
+		Keyboard.row('Назад')
+		bot.send_message(message.from_user.id, 'Четверг', reply_markup = Keyboard)
+
+	if message.text == 'Пятница' and database.chet == 'Чет':
+		Keyboard = telebot.types.ReplyKeyboardMarkup(True, False)
+		Keyboard.row('Нет - Элементы высшей математики')
+		Keyboard.row('Нет - БЖ')
+		Keyboard.row('Нет - Устройство и функционирование ИС')
+		Keyboard.row('Назад')
+		bot.send_message(message.from_user.id, 'Пятница', reply_markup = Keyboard)
+
+	if message.text == 'Субота' and database.chet == 'Чет':
+		Keyboard = telebot.types.ReplyKeyboardMarkup(True, False)
+		Keyboard.row('113 - Основы алгоритмизации и программирования')
+		Keyboard.row('309 - Русский язык и культура речи')
+		Keyboard.row('Нет - МДК')
+		Keyboard.row('Назад')
+		bot.send_message(message.from_user.id, 'Субота', reply_markup = Keyboard)
+
+
+	if message.text == 'Понедельник' and database.chet == 'Нечет':
+		Keyboard = telebot.types.ReplyKeyboardMarkup(True, False)
+		Keyboard.row('317 - Элементы высшей математики')
+		Keyboard.row('316 - МДК')
+		Keyboard.row('301/2 - Устройство и функционирование ИС')
+		Keyboard.row('Назад')
+		bot.send_message(message.from_user.id, 'Понедельник', reply_markup = Keyboard)
+
+	if message.text == 'Вторник' and database.chet == 'Нечет':
+		Keyboard = telebot.types.ReplyKeyboardMarkup(True, False)
+		Keyboard.row('301/3 - Основы алгоритмизации и программирования')
+		Keyboard.row('301/2 - МДК')
+		Keyboard.row('316 - Элементы высшей математики')
+		Keyboard.row('Назад')
+		bot.send_message(message.from_user.id, 'Вторник', reply_markup = Keyboard)
+
+	if message.text == 'Среда' and database.chet == 'Нечет':
+		Keyboard = telebot.types.ReplyKeyboardMarkup(True, False)
+		Keyboard.row('301/2 - Устройство и функционирование ИС')
+		Keyboard.row('401 - Физра')
+		Keyboard.row('316 - Элементы высшей математики')
+		Keyboard.row('Назад')
+		bot.send_message(message.from_user.id, 'Среда', reply_markup = Keyboard)
+
+	if message.text == 'Четверг' and database.chet == 'Нечет':
+		Keyboard = telebot.types.ReplyKeyboardMarkup(True, False)
+		Keyboard.row('113-МДК')
+		Keyboard.row('107/5 - Инглишь')
+		Keyboard.row('113 - Основы алгоритмизации и программирования')
+		Keyboard.row('Назад')
+		bot.send_message(message.from_user.id, 'Четверг', reply_markup = Keyboard)
+
+	if message.text == 'Пятница' and database.chet == 'Нечет':
+		Keyboard = telebot.types.ReplyKeyboardMarkup(True, False)
+		Keyboard.row('310 - Элементы высшей математики')
+		Keyboard.row('407 - БЖ')
+		Keyboard.row('412 - Устройство и функционирование ИС')
+		Keyboard.row('Назад')
+		bot.send_message(message.from_user.id, 'Пятница', reply_markup = Keyboard)
+
+	if message.text == 'Субота' and database.chet == 'Нечет':
+		Keyboard = telebot.types.ReplyKeyboardMarkup(True, False)
+		Keyboard.row('113 - Основы алгоритмизации и программирования')
+		Keyboard.row('309 - Русский язык и культура речи')
+		Keyboard.row('301/2 - МДК')
+		Keyboard.row('Назад')
+		bot.send_message(message.from_user.id, 'Субота', reply_markup = Keyboard)
+
+	if message.text == '107/5 - Инглишь':
+		Keyboard = telebot.types.ReplyKeyboardMarkup(True, False)
+		Keyboard.row('Кайфеджан','Тихонова')
+		Keyboard.row('Назад')
+		bot.send_message(message.chat.id, 'Какой группы?', reply_markup = Keyboard)
+
+
+		global DZ_NUM
+		DZ_NUM = 0
+
+	if message.text == '113 - Основы алгоритмизации и программирования' and '301/3 - Основы алгоритмизации и программирования':
+		DZ_NUM = 0
+		bot.send_message(message.chat.id,database.lesson[DZ_NUM])
+
+	if message.text == '310/3 - Устройство и функционирование ИС' and '301/2 - Устройство и функционирование ИС' and '412 - Устройство и функционирование ИС':
+		DZ_NUM = 1
+		bot.send_message(message.chat.id,database.lesson[DZ_NUM]) 
+
+	if message.text == '23 - Элементы высшей математики' and '22 - Элементы высшей математики' and 'Нет - Элементы высшей математики' and '317 - Элементы высшей математики' and '316 - Элементы высшей математики' and '310 - Элементы высшей математики':
+		DZ_NUM = 2
+		bot.send_message(message.chat.id,database.lesson[DZ_NUM])
+
+	if message.text == '309 - Русский язык и культура речи':
+		DZ_NUM = 3
+		bot.send_message(message.chat.id,database.lesson[DZ_NUM])
+
+	if message.text == '301/2 - МДК' and 'Нет-МДК' and '316 - МДК' and '113-МДК':
+		DZ_NUM = 4
+		bot.send_message(message.chat.id,database.lesson[DZ_NUM])
+
+	if message.text == 'Нет - БЖ' and '407 - БЖ':
+		DZ_NUM = 5
+		bot.send_message(message.chat.id,database.lesson[DZ_NUM])
+
+	if message.text == 'Кайфеджан':
+		DZ_NUM = 6
+		bot.send_message(message.chat.id,database.lesson[DZ_NUM])
+
+	if message.text == 'Тихонова':
+		DZ_NUM = 7
+		bot.send_message(message.chat.id,database.lesson[DZ_NUM])
+
+
+	if message.text == 'Админ на месте':
+		Keyboard = telebot.types.ReplyKeyboardMarkup(True, False)
+		Keyboard.row('Основы алгоритмов и программирования#')
+		Keyboard.row('Устройство и функционирование ИС#')
+		Keyboard.row('Элементы высшей математики#')
+		Keyboard.row('Русский язык и культура речи#')
+		Keyboard.row('Инглишь#')
+		Keyboard.row('МДК#')
+		Keyboard.row('БЖ#')
+		Keyboard.row('Назад')
+		bot.send_message(message.chat.id, 'На какой предмет?', reply_markup = Keyboard)
+
+	if message.text == 'Основы алгоритмов и программирования#':
+		DZ_NUM = 0
+		sent = bot.send_message(message.chat.id,'Введи задание')
+		bot.register_next_step_handler(sent, lesson)
+
+	if message.text == 'Устройство и функционирование ИС#':
+		DZ_NUM = 1
+		sent = bot.send_message(message.chat.id,'Введи задание')
+		bot.register_next_step_handler(sent, lesson)
+
+	if message.text == 'Элементы высшей математики#':
+		DZ_NUM = 2
+		sent = bot.send_message(message.chat.id,'Введи задание')
+		bot.register_next_step_handler(sent, lesson)
+
+	if message.text == 'Русский язык и культура речи#':
+		DZ_NUM = 3
+		sent = bot.send_message(message.chat.id,'Введи задание')
+		bot.register_next_step_handler(sent, lesson)
+
+	if message.text == 'МДК#':
+		DZ_NUM = 4
+		sent = bot.send_message(message.chat.id,'Введи задание')
+		bot.register_next_step_handler(sent, lesson)
+
+	if message.text == 'БЖ#':
+		DZ_NUM = 5
+		sent = bot.send_message(message.chat.id,'Введи задание')
+		bot.register_next_step_handler(sent, lesson)
+
+	if message.text == 'Инглишь':
+		DZ_NUM = 6
+		sent = bot.send_message(message.chat.id,'Введи задание')
+		bot.register_next_step_handler(sent, lesson)
+
+	if message.text == 'Илья яойщик':
+		DZ_NUM = 7
+		sent = bot.send_message(message.chat.id,'Введи задание')
+		bot.register_next_step_handler(sent, lesson)
+
+
+	if message.text == 'Назад':
+		bot.send_message(message.from_user.id, 'На какой день недели тебе нужно рассписание?', reply_markup = menu)
+
+
+	if message.text == 'Чнг':
+		sent = bot.send_message(message.chat.id,'Введи изменения')
+		bot.register_next_step_handler(sent,change1)
+
+	if message.text == 'Изменения':
+		bot.send_message(message.chat.id,database.change)
+
+
+def chet(message):
+	database.chet = message.text
+	return database.chet
+
+def lesson(message):
+	database.lesson[DZ_NUM] = message.text
+	return database.lesson[DZ_NUM]
 
 def change1(message):
-    database.change=message.text
-    return database.change
+	database.change = message.text
+	return database.change
+
 bot.polling()
